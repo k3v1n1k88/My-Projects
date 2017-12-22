31a131a3b3118e6acabe90536a9e01e7627b7c89
k3v1n1k88 <nkevin1088@gmail.com>
2017-12-22 15:38:52 +0700

    Add files via upload
    pre-final

--- Socket_WebClient/Proxy2/Proxy2.cpp
+++ Socket_WebClient/Proxy2/Proxy2.cpp
@@ -9,31 +9,60 @@
 #include <fstream>
 #include <windows.h>
 #include "TimeOutSocket.h"
+#include <thread>
+#include <vector>
+#include <windows.h>
+#include <tchar.h>
+#include <strsafe.h>
+
+
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
-#define forbiden "<!DOCTYPE..><html>\r\n<head><title>403 Forbiden</title></head>\r\n<body>\r\n<h1>Forbiden 403</h1>\r\n<p>You don't have permission to host %s</p>\r\n</body>\r\n</html>"
-#define pagenotfound "<!DOCTYPE..><html>\r\n<head><title>401 Page not found</title></head>\r\n<body>\r\n<font size=\"25\"><b>401 Page not found</b></font>\r\n<p>Your request URL was not found</p>\r\n<p>Please check again</p>\r\n<hr>\r\n<p align=\"center\" >Proxy create by k3v1n1k88</p>\r\n</body>\r\n</html>"
+// Trang web forbiden
+#define forbiden "<!DOCTYPE html>\r\n<html>\r\n<head><title>403 Forbiden</title></head>\r\n<body>\r\n<h1>Forbiden 403</h1>\r\n<p>You don't have permission to host %s</p>\r\n</body>\r\n</html>"
+
+// Trang web not found
+#define pagenotfound "<!DOCTYPE html>\r\n<html>\r\n<head><title>401 Page not found</title></head>\r\n<body>\r\n<font size=\"25\"><b>401 Page not found</b></font>\r\n<p>Your request URL was not found</p>\r\n<p>Please check again</p>\r\n<hr>\r\n<p align=\"center\" >Proxy create by k3v1n1k88</p>\r\n</body>\r\n</html>"
+//#define https "<!DOCTYPE html>\r\n<html>\r\n<head><title>Sorry, we do not support for HTTPS</title></head>\r\n<body>\r\n<font size=\"25\"><b>401 Page not reach</b></font>\r\n<p>Your request is not support</p>\r\n<p>Please try other</p>\r\n<hr>\r\n<p align=\"center\" >Proxy create by k3v1n1k88</p>\r\n</body>\r\n</html>"
 //#define forbiden "<html><head><title>403 Forbiden</title></head>\n<body><h1>Forbiden 403</h1>\n<p>You don't have permission to access/on this website</p>\n</body>\n</html>"
 // The one and only application object
+#define MAX_THREADS 5
 
 CWinApp theApp;
 
 using namespace std;
 
+typedef struct MyData{
+    SOCKET *hcs;
+} MYDATA, *PMYDATA;
+
+//Dung de Get IP tu 1 host cho truoc
 char* GetIP(char* host);
+
+//Dung de lay ten cua host tu lenh GET
 char* GetNameHost(const char *get);
+
+//Chuyen tu char* sang LPCWSTR
 wchar_t *convertCharArrayToLPCWSTR(const char* charArray)
 {
 	wchar_t* wString = new wchar_t[4096];
 	MultiByteToWideChar(CP_ACP, 0, charArray, -1, wString, 4096);
 	return wString;
 }
+
+//Check black list
+BOOL CheckBlackList(fstream &fin, char* host, CSocket &cs);
+DWORD WINAPI BeginProcess(LPVOID lp);
+
+
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
 	int nRetCode = 0;
-
+	PMYDATA pDataArray[MAX_THREADS];
+    DWORD   dwThreadIdArray[MAX_THREADS];
+    HANDLE  hThreadArray[MAX_THREADS];
 	HMODULE hModule = ::GetModuleHandle(NULL);
 
 	if (hModule != NULL)
@@ -54,150 +83,68 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 			}
 			
 
-			CSocket csServer;
-			CSocket csClient;
-			CSocket csWebsite;
-	
-			if(csServer.Create(8888,SOCK_STREAM,L"127.0.01")==false){
+			CSocket csServer;	// Socket Server
+			SOCKET hSocket[10];	
+
+
+			if(csServer.Create(8888,SOCK_STREAM,L"127.0.0.1")==false){
 				cout<<"Khoi tao socket that bai..."<<endl;
 				cout<<csServer.GetLastError();
 				return 1;
 			}
 
-			if(csServer.Listen()==false){
+			if(csServer.Listen(10)==false){
 				cout<<"Khong the lang nghe tren port nay"<<endl;
 			}
-			
-			//Begin while(1)
+
+			// Bat dau chay server voi 5 luong
 			while(1){
-				string buffer;
-				char* pstrBufferReceiveFromWeb;
-				char* buff;
-				char* get;
-				char *host;
-				char *ip;
-				fstream fin;
-				fstream fout;
-				fout.open("output.txt",ios_base::out);
-				cout<<"/* DANG LANG NGHE 1 KET NOI..."<<endl<<endl;
-
-				
-				if(csServer.Accept(csClient)==true){
-					cout<<"Client da ket noi..."<<endl;				
-					int count = 1;
-					while(count){
-						buff = new char[1000];
-						count = csClient.Receive(buff,1000,0);
-						buffer.append(buff,count);
-						if(count<1000)
-							break;
+				CSocket *csClient = new CSocket;
+				for(int i=0;i<10;i++){
+					if(csServer.Accept(*csClient)){
+						cout<<"Client da ket noi..."<<endl;
+						hSocket[i] = csClient ->Detach();
+						hThreadArray[i] = CreateThread(NULL, 0,BeginProcess,(LPVOID)&hSocket[i],0,&dwThreadIdArray[0]);
 					}
-					cout<<buffer<<endl;
-					if(buffer.size()!=0){
-						//Build GET instruction from GET browser
-						get = new char[buffer.size()+1];
-						memcpy(get,buffer.c_str(),buffer.size()+1);
-
-						host = GetNameHost(get);
-						ip = GetIP(host);
-						cout<<"Host : "<<host<<endl;
-						if(ip == NULL){
-							// when IP is not avaible we don't process it
-							cout<<"IP: don't know! Please check again"<<endl;
-							csClient.Send(pagenotfound,strlen(pagenotfound),0);
-						}
-						
-						// Begin work with IP available
-						else{
-							string strReceiveFromWeb;
-							pstrBufferReceiveFromWeb = new char [1000];
-							cout<<"IP: "<<ip<<endl;
-							fin.open("blacklist.txt",ios_base::in);
-							// Xu li website bi cam
-							if(fin.is_open()){
-								while(!fin.eof()){
-									char blacklist[100];
-									fin>>blacklist;
-									if(strstr(host,blacklist)!=NULL){
-										char *buildweb;
-										buildweb = new char [strlen(forbiden)+strlen(host)-1];
-										sprintf_s(buildweb, strlen(buildweb)+1, forbiden,host);
-										csClient.Send(buildweb,strlen(buildweb),0);
-										break;
-									}
-								}
-							}
-							//Xu li website k bi cam
-							else{
-								csWebsite.Create();
-							
-								if(csWebsite.Connect(convertCharArrayToLPCWSTR(ip),80)<0){
-									ASSERT(FALSE);
-									cout<<"Counld't connect!"<<endl;
-									csClient.Send(pagenotfound,strlen(pagenotfound),0);
-								}
-								else{
-									int num = 1;
-
-									//Send request
-									csWebsite.Send(get,strlen(get),0);
-									while(num){
-										//timeval timeout = { 5, 0 };
-										//fd_set in_set;
-
-										//FD_ZERO(&in_set);
-										//FD_SET(csWebsite, &in_set);
-
-										//// select the set
-										//int cnt = select(csWebsite + 1, &in_set, NULL, NULL, &timeout);
-										CTimeOutSocket cTimeOut;
-										if(!cTimeOut.SetTimeOut(3000)){
-											ASSERT(FALSE);
-										}
-										if((num = csWebsite.Receive(pstrBufferReceiveFromWeb,1024,0))<0){
-											Sleep(1000);
-										}
-										if(num == -1){
-											csWebsite.Send("",0,0);
-											break;
-										}
-										if(!cTimeOut.KillTimeOut()){
-											ASSERT(FALSE);
-										}
-										//cout<<strReceiveFromWeb;
-										if(num!=0){
-											strReceiveFromWeb.append(pstrBufferReceiveFromWeb,num);
-											csClient.Send(strReceiveFromWeb.c_str(),num,0);
-											fout<<strReceiveFromWeb;
-											strReceiveFromWeb.clear();		
-										}
-										if(num == 0 ){
-											csWebsite.Send("",0,0);
-											break;
-										}
-									}
-								}
-								csWebsite.ShutDown();
-								csWebsite.Close();
-								//delete pstrBufferReceiveFromWeb;
-							}
-						
-						}//End work with website available
-						
-						delete get;
-					}
-					//delete host;
-					//delete buildweb;
-					//delete buildweb;
-					csClient.ShutDown();
-					csClient.Close();
-					delete buff;
-					buffer.clear();
-					cout<<"...KET THUC KET NOI*/"<<endl<<endl;
-					fout.close();
-					fin.close();
 				}
-			}//end while(1)
+				WaitForMultipleObjects(10, hThreadArray, TRUE, INFINITE);
+				delete csClient;
+			}
+				//for(int i=0;i<MAX_THREADS;i++){
+				//	//csServer.Attach(csClient[i]);
+				//	cout<<"DANG CHO MOT KET NOI..."<<endl;
+				//	CSocket *csClient = new CSocket;
+				//	if(csServer.Accept(*csClient)==TRUE){
+				//		//csClient[i].Detach();
+
+				//		hSocket[i] = csClient->Detach();
+				//		/*pDataArray[i] = (PMYDATA) HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY,sizeof(MYDATA));
+				//		pDataArray[i]->hcs = &hSocket[i];*/
+				//		hThreadArray[i] = CreateThread(NULL, 0,(LPTHREAD_START_ROUTINE) BeginProcess,(LPVOID)hSocket[i],0,&dwThreadIdArray[i]);
+				//		if(hThreadArray[i]==NULL){
+				//			cout<<"Khong the tao thread "<<i<<" "<<endl;
+				//		}
+				//		
+				//		//csClient[i].ShutDown();
+				//		//csClient[i].Close();
+				//		//csClient[i].Detach();
+				//	}
+				//	
+				//}
+				//WaitForMultipleObjects(MAX_THREADS, hThreadArray, TRUE, INFINITE);
+				//for(int i=0; i<MAX_THREADS; i++)
+				//{
+				//	
+				//	CloseHandle(hThreadArray[i]);
+				//	if(pDataArray[i] != NULL)
+				//	{
+				//		HeapFree(GetProcessHeap(), 0, pDataArray[i]);
+				//		pDataArray[i] = NULL;    // Ensure address is not reused.
+				//	}
+				//}
+			//}
+			
+			return 0;
 		}
 	}
 	else
@@ -239,4 +186,155 @@ char* GetNameHost(const char *get){
 	host = strtok_s(pfind," ",&temp);
 	host = strtok_s(NULL,"\r\n: ",&temp);
 	return host;
+}
+
+BOOL CheckBlackList(fstream &fin, char* host, CSocket *cs){
+	while(!fin.eof()){
+		char blacklist[100];
+		fin>>blacklist;
+		if(strstr(host,blacklist)!=NULL){
+			char *buildweb;
+			buildweb = new char [strlen(forbiden)+strlen(host)-1];
+			sprintf_s(buildweb, strlen(buildweb)+1, forbiden,host);
+			cs->Send(buildweb,strlen(buildweb),0);
+			return TRUE;
+		}
+	}
+	return FALSE;
+}
+
+DWORD WINAPI BeginProcess(LPVOID lp){
+	SOCKET hcs =*(SOCKET*) lp;
+	CSocket *csClient = new CSocket;
+	csClient->Attach(hcs);
+	cout<<"Dang nhan du lieu tu browser..."<<endl;
+	char* buff;
+	CSocket csWebsite;
+	fstream fin;
+	fstream fout;
+	string buffer;
+	char *host =new char;
+	char *ip =new char;
+	char* get=new char;
+	char* pstrBufferReceiveFromWeb = new char;
+	fout.open("output.txt",ios_base::out);
+	cout<<"Client da ket noi..."<<endl;				
+	int count = 1;
+	while(count){
+		buff = new char[1000];
+		count = csClient->Receive(buff,1000,0);
+		buffer.append(buff,count);
+		if(count<1000)
+			break;
+	}
+	//cout<<buffer<<endl;
+	if(buffer.size()!=0){
+		//sua lai thanh 1.0
+		if(buffer.find("HTTP/1.1")!=string::npos){
+			buffer.replace(buffer.find("HTTP/1.1"),8,"HTTP/1.0");
+		}
+		//Sua connection closed
+		if(buffer.find("css")==string::npos){
+			if(buffer.find("keep-alive")!=string::npos){
+				buffer.replace(buffer.find("keep-alive"),10,"Closed");
+			}
+			if(buffer.find("keep-alive")!=string::npos){
+				buffer.replace(buffer.find("keep-alive"),10,"Closed");
+			}
+		}
+		//Build GET instruction from GET browser
+		get = new char[buffer.size()+1];
+		memcpy(get,buffer.c_str(),buffer.size()+1);
+		cout<<get;
+		host = GetNameHost(get);
+		ip = GetIP(host);
+		cout<<"Host : "<<host<<endl;
+		if(ip == NULL){
+			// when IP is not avaible we don't process it
+			cout<<"IP: don't know! Please check again"<<endl;
+			csClient->Send(pagenotfound,strlen(pagenotfound),0);
+		}
+		// Begin work with IP available
+		else{
+			string strReceiveFromWeb = *(new string);
+			pstrBufferReceiveFromWeb = new char [1000];
+			cout<<"IP: "<<ip<<endl;
+			fin.open("blacklist.txt",ios_base::in);
+			// Xu li website bi cam
+			if(fin.is_open()&&CheckBlackList(fin,host,csClient)){
+				/*while(!fin.eof()){
+					char blacklist[100];
+					fin>>blacklist;
+					if(strstr(host,blacklist)!=NULL){
+						char *buildweb;
+						buildweb = new char [strlen(forbiden)+strlen(host)-1];
+						sprintf_s(buildweb, strlen(buildweb)+1, forbiden,host);
+						csClient.Send(buildweb,strlen(buildweb),0);
+						break;
+					}
+				}*/
+		
+			}
+			//Xu li website k bi cam
+			else{
+				csWebsite.Create();
+	
+				if(csWebsite.Connect(convertCharArrayToLPCWSTR(ip),80)<0){
+					ASSERT(FALSE);
+					cout<<"Counld't connect!"<<endl;
+					csClient->Send(pagenotfound,strlen(pagenotfound),0);
+				}
+				else{
+					int num = 1;
+
+					//Send request
+					csWebsite.Send(get,strlen(get),0);
+					while(num){
+						// Cac cTimeOut muc dich de set timeoout cho socket khi cho` qua lau
+						CTimeOutSocket cTimeOut;
+						if(!cTimeOut.SetTimeOut(3000)){
+							ASSERT(FALSE);
+						}
+						num = csWebsite.Receive(pstrBufferReceiveFromWeb,1000,0);
+						int nError = GetLastError();
+						if(nError == WSAEWOULDBLOCK)
+							num = 0;
+				
+						if(num == -1){
+							break;
+						}
+
+						if(!cTimeOut.KillTimeOut()){
+							ASSERT(FALSE);
+						}
+						//cout<<strReceiveFromWeb;
+						if(num!=0){
+							strReceiveFromWeb.append(pstrBufferReceiveFromWeb,num);
+							csClient->Send(strReceiveFromWeb.c_str(),num,0);
+							fout<<strReceiveFromWeb;
+							strReceiveFromWeb.clear();
+						}
+					}
+				}
+				csWebsite.ShutDown();
+				csWebsite.Close();
+				delete pstrBufferReceiveFromWeb;
+			}
+	
+		}//End work with website available
+	
+		delete get;
+	}
+	//delete host;
+	//delete buildweb;
+	//delete buildweb;
+	csClient->ShutDown();
+	csClient->Close();
+	delete buff;
+	buffer.clear();
+	cout<<"...KET THUC KET NOI*/"<<endl<<endl;
+	fout.close();
+	fin.close();	
+	return 0;
+	csClient->Detach();
 }
